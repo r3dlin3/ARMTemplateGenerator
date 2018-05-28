f38e3355202ce8c82d99b7fdba5e71ecad342bfb
Yannick Beot - ADM <beotya-adm@ldc.com>
2018-05-28 16:56:38 +0200

    Add App Service

--- plopfile.js
+++ plopfile.js
@@ -35,7 +35,7 @@ module.exports = function (plop) {
         return '';
     });
 
-    // create your generators here
+    // Generators
     plop.setGenerator('VM', {
         description: 'This module generates ARM template file for a VM',
         prompts: [
@@ -379,5 +379,63 @@ module.exports = function (plop) {
                 type: "printHelpDeployment"
             }
         ]  // array of actions
-    })
+    });
+
+    plop.setGenerator('AppService', {
+        description: 'This module generates ARM template file for an App Service',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the template name?',
+                validate: validateRequired
+            },            
+            {
+                type: 'confirm',
+                name: 'existingASP',
+                default: false,
+                message: 'Do you want to use an existing App Service Plan?'
+            },
+            {
+                type: 'confirm',
+                name: 'createSlot',
+                default: false,
+                message: 'Do you want to create slots?',
+            },
+            {
+                type: 'confirm',
+                name: 'createAppInsights',
+                default: false,
+                message: 'Do you want to create an App Insights?',
+            },
+            {
+                type: 'confirm',
+                name: 'createCDN',
+                default: false,
+                message: 'Do you want to create a CDN?',
+            },
+            {
+                type: 'confirm',
+                name: 'createCache',
+                default: false,
+                message: 'Do you want to create a Redis cache?',
+            },
+            
+        ], // array of inquirer prompts
+        actions: [
+            {
+                type: "add",
+                path: "generated/{{name}}.parameters.json",
+                templateFile: "templates/AppService/azuredeploy.parameters.json"
+            },
+            {
+                type: "add",
+                path: "generated/{{name}}.json",
+                templateFile: "templates/AppService/azuredeploy.json"
+            },
+            {
+                type: "printHelpDeployment"
+            }
+        ]  // array of actions
+    });
 };
