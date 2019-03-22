6b3a8468212117a80a99f5a62c7fc6d5571688bb
r3dlin3 <r3dlin3@hotmail.com>
2019-03-22 16:38:26 +0100

    rely on actionTypes
    Comments introduced in 2.2 are not used as a comment cannot be displayed without code.
    answers object is not available

--- templates/DataLakeStore/component.js
+++ templates/DataLakeStore/component.js
@@ -2,48 +2,43 @@ var validateRequired = require('../../validator').validateRequired;
 const chalk = require('chalk');
 
 module.exports = function (plop) {
-plop.setActionType('printHelpDeployment', function (answers, config, plop) {
-    // do something
-    console.log(chalk.yellow('To deploy your template, use the following PowerShell cmdlet:'));
-    console.log(chalk.yellow('New-AzureRmResourceGroupDeployment -ResourceGroupName <ResourceGroupName> -verbose -TemplateFile .\\generated\\' + answers.name + '.json -TemplateParameterFile .\\generated\\' + answers.name + '.parameters.json'));
-    return '';
-});
-plop.setGenerator('DataLakeStore', {
-    description: 'This module generates ARM template file for a Data Lake Store',
-    prompts: [
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the template name?',
-            validate: validateRequired
-        },
-        {
-            type: 'confirm',
-            name: 'createFirewallRules',
-            default: true,
-            message: 'Do you want to create firewall rules?'
-        },
-        {
-            type: 'confirm',
-            name: 'useKeyVault',
-            default: true,
-            message: 'Do you want to use a Key Vault for encryption?'
-        }
-    ], // array of inquirer prompts
-    actions: [
-        {
-            type: "add",
-            path: "generated/{{name}}.parameters.json",
-            templateFile: "./azuredeploy.parameters.json"
-        },
-        {
-            type: "add",
-            path: "generated/{{name}}.json",
-            templateFile: "./azuredeploy.json"
-        },
-        {
-            type: "printHelpDeployment"
-        }
-    ]  // array of actions
-});
+    plop.load('../../actionTypes.js');
+    plop.setGenerator('DataLakeStore', {
+        description: 'This module generates ARM template file for a Data Lake Store',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the template name?',
+                validate: validateRequired
+            },
+            {
+                type: 'confirm',
+                name: 'createFirewallRules',
+                default: true,
+                message: 'Do you want to create firewall rules?'
+            },
+            {
+                type: 'confirm',
+                name: 'useKeyVault',
+                default: true,
+                message: 'Do you want to use a Key Vault for encryption?'
+            }
+        ], // array of inquirer prompts
+        actions: [
+            {
+                type: "add",
+                path: "generated/{{name}}.parameters.json",
+                templateFile: "./azuredeploy.parameters.json"
+            },
+            {
+                type: "add",
+                path: "generated/{{name}}.json",
+                templateFile: "./azuredeploy.json"
+            },
+            {
+                type: "printHelpDeployment"
+            }
+        ]  // array of actions
+    });
 }
