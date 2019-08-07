480cc857d4f7f4af0fece84b8915bea9f7ea182d
r3dlin3 <r3dlin3@hotmail.com>
2019-08-07 14:30:01 +0200

    add az module

--- actionTypes.js
+++ actionTypes.js
@@ -4,7 +4,10 @@ module.exports = function (plop) {
     plop.setActionType('printHelpDeployment', function (answers, config, plop) {
         // do something
         console.log(chalk.yellow('To deploy your template, use the following PowerShell cmdlet:'));
+        console.log(chalk.yellow('1) Using AzureRM module'));
         console.log(chalk.yellow('New-AzureRmResourceGroupDeployment -ResourceGroupName <ResourceGroupName> -verbose -TemplateFile .\\generated\\' + answers.name + '.json -TemplateParameterFile .\\generated\\' + answers.name + '.parameters.json'));
+        console.log(chalk.yellow('2) Using Az module'));
+        console.log(chalk.yellow('New-AzResourceGroupDeployment -ResourceGroupName <ResourceGroupName> -verbose -TemplateFile .\\generated\\' + answers.name + '.json -TemplateParameterFile .\\generated\\' + answers.name + '.parameters.json'));
         return '';
     });
 
