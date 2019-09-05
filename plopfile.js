4353048027accf4972cd1adbaa0fe4b4c23c458b
r3dLiN3 <r3dlin3@hotmail.com>
2019-09-05 19:01:14 +0200

    cleanup setHelper was not usable XXX Need refactoring

--- plopfile.js
+++ plopfile.js
@@ -1,25 +1,7 @@
-var repeatHelper = require('handlebars-helper-repeat');
 var glob = require("glob")
 const chalk = require('chalk');
 
 module.exports = function (plop) {
-    // helpers for template generation
-    plop.setHelper('repeat', repeatHelper);
-    plop.setHelper('is', function (arg1, arg2, opts) {
-        if (arg1 == arg2) {
-            return opts.fn(this)
-        } else {
-            return opts.inverse(this)
-        }
-    });
-    plop.setHelper('isnot', function (arg1, arg2, opts) {
-        if (arg1 != arg2) {
-            return opts.fn(this)
-        } else {
-            return opts.inverse(this)
-        }
-    });
-
     // Generators
     var files = glob.sync("./templates/*/component.js");
     plop.load(files);
