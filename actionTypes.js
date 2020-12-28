d3a869a3a62b10c9000bace9daea24b6a768f57e
r3dLiN3 <r3dlin3@hotmail.com>
2020-12-28 20:04:18 +0100

    add az cli command

--- actionTypes.js
+++ actionTypes.js
@@ -21,11 +21,15 @@ module.exports.DEFAULT_ACTIONS = function (path) {
 };
 
 module.exports.printHelpDeployment = function (answers, config, plop) {
-    // do something
-    console.log(chalk.yellow('To deploy your template, use the following PowerShell cmdlet:'));
-    console.log(chalk.yellow('1) Using AzureRM module'));
-    console.log(chalk.yellow('New-AzureRmResourceGroupDeployment -ResourceGroupName <ResourceGroupName> -verbose -TemplateFile .\\generated\\' + answers.name + '.json -TemplateParameterFile .\\generated\\' + answers.name + '.parameters.json'));
-    console.log(chalk.yellow('2) Using Az module'));
-    console.log(chalk.yellow('New-AzResourceGroupDeployment -ResourceGroupName <ResourceGroupName> -verbose -TemplateFile .\\generated\\' + answers.name + '.json -TemplateParameterFile .\\generated\\' + answers.name + '.parameters.json'));
+
+    console.log(chalk.yellow('To deploy your template, use:'));
+    console.log(chalk.yellow('1) the following PowerShell cmdlet:'));
+    console.log(chalk.yellow('\t1) Using AzureRM module'));
+    console.log(chalk.yellow('\tNew-AzureRmResourceGroupDeployment -ResourceGroupName <ResourceGroupName> -verbose -TemplateFile .\\generated\\' + answers.name + '.json -TemplateParameterFile .\\generated\\' + answers.name + '.parameters.json'));
+    console.log(chalk.yellow('\t2) Using Az module'));
+    console.log(chalk.yellow('\tNew-AzResourceGroupDeployment -ResourceGroupName <ResourceGroupName> -verbose -TemplateFile .\\generated\\' + answers.name + '.json -TemplateParameterFile .\\generated\\' + answers.name + '.parameters.json'));
+    console.log(chalk.yellow('2) the Az CLI :'));
+    console.log(chalk.yellow('az deployment group create --resource-group <ResourceGroupName> --template-file .\\generated\\' + answers.name + '.json --parameters @.\\generated\\' + answers.name + '.parameters.json'));
+
     return '';
 };
